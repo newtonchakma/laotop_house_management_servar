f20e60490f9cf7a5afa639acda9256d74c41f03f
newtonchakma <newtonchakma304@gmail.com>
2022-05-07 22:16:39 +0600

    get single data by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -26,6 +26,12 @@ async function run(){
             const cursor = laptopCollection.find(query);
             const laptops = await cursor.toArray();
             res.send(laptops);
+        });
+        app.get('/laptop/:id', async(req, res) =>{
+            const id  = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const laptop = await laptopCollection.findOneAndDelete(query);
+            res.send(laptop)
         })
     }
 
